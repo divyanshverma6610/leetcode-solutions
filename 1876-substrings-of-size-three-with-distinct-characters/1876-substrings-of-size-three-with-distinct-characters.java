6808810e6b881b61a891bc953846273fbbfd1a06
Divyansh <divyanshverma6610@gmail.com>
2026-09-02 11:08:32 +0530

    Time: 1 ms (96.45%), Space: 42.8 MB (67.33%) - LeetHub

---
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countGoodSubstrings(String s) {
+        int count = 0;
+        for (int i = 0; i <= s.length() - 3; i++) {
+            if (s.charAt(i) != s.charAt(i + 1) && s.charAt(i) != s.charAt(i + 2) && s.charAt(i + 1) != s.charAt(i + 2))
+                count++;
+        }
+        return count;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
